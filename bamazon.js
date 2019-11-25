2648666198d6b0147fe5847fe59513c4996f0149
David Henderson <dhend1863@gmail.com>
2019-11-25 14:43:18 -0600

    Bug Fixed

--- bamazon.js
+++ bamazon.js
@@ -34,17 +34,45 @@ var promptCustomer = function (res) {
         type: "input",
         name: "choice",
         message: "What would you like to buy? [Quit with Q]"
-        }]).then(function(answer){
-            var correct = false;
-            for(var i =0; i<res.length;i++){
-                if(res[i].productname===answer.choice){
-                 correct = true;
-                 var product=answer.choice;
-                 var id=i;
-                }
+    }]).then(function (answer) {
+        var correct = false;
+        if(answer.choice.toUpperCase()=="Q");
+        for (var i = 0; i < res.length; i++) {
+            if (res[i].productname === answer.choice) {
+                correct = true;
+                var product = answer.choice;
+                var id = i;
+                inquirer.prompt({
+                    type: "input",
+                    name: "quant",
+                    message: "How many would you like?",
+                    validate: function (value) {
+                        if (isNaN(value) == false) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+
+                }).then(function (answer) {
+                    if((res[id].stockquantity-answer.quant)>0){
+                        connection.query("UPDATE products SET stockquantity='"+(res[id].stockquantity-answer.quant)+"' WHERE productname='"+product+"'", function(err,res2){
+                            console.log("Product Bought!");
+                            makeTable();
+                        })
+                    } else {
+                        console.log("HMMMM thats not a selection");
+                        promptCustomer(res);
+                    }
+
+                })
+
             }
 
-        })
+        }
+
+
+    })
 
 }
 
